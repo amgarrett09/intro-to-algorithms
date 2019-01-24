4081c76d8a6b54f3704691cf53e7cd3dbf0f34d9
Alex Garrett <agarrettR8@gmail.com>
2019-01-24 13:19:12 -0500

    added bounds checking

--- peak-finding.js
+++ peak-finding.js
@@ -9,6 +9,9 @@ const arr5 = [8, 7, 9, 8, 8, 9, 0, 5, 2, 1];
 const arr6 = [8, 7, 9, 8, 8, 9, 6, 5, 2, 1];
 const arr7 = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const arr8 = [7, 6, 5, 4, 3, 2, 1, 0];
+const arr9 = [1];
+const arr10 = [1, 2];
+const arr11 = [1, 2, 3];
 
 /* Binary search algorithm. Complexity is O(log(n)) */
 function find1DPeak(arr) {
@@ -17,9 +20,9 @@ function find1DPeak(arr) {
     let index;
     while (left <= right) {
         index = left + Math.floor((right - left) / 2);
-        if (arr[index - 1] > arr[index]) {
+        if (index > 0 && arr[index - 1] > arr[index]) {
             right = index - 1;
-        } else if (arr[index + 1] > arr[index]) {
+        } else if (index < arr.length - 1 && arr[index + 1] > arr[index]) {
             left = index + 1;
         } else {
             return arr[index];
@@ -36,3 +39,6 @@ console.log("Expected 5, received " + find1DPeak(arr5).toString());
 console.log("Expected 9, received " + find1DPeak(arr6).toString());
 console.log("Expected 8, received " + find1DPeak(arr7).toString());
 console.log("Expected 7, received " + find1DPeak(arr8).toString());
+console.log("Expected 1, received " + find1DPeak(arr9).toString());
+console.log("Expected 2, received " + find1DPeak(arr10).toString());
+console.log("Expected 3, received " + find1DPeak(arr11).toString());
